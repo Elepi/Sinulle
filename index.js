332d616b9fa940a4fd3babf985a85f752b90ceb1
diegolopez03 <diego232.lopez@gmail.com>
2020-11-03 15:26:26 -0600

    Implementación de cookie parser para crear la sesión de usuario y de flash para mostrar mensajes al usuario

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Importar los módulos requeridos para el funcionamiento del servidor
+const mongoose = require("mongoose");
 const express = require("express");
 //llamar la configuracion de la base
 require("./config/db");
@@ -6,6 +7,10 @@ const exphbs = require("express-handlebars");
 const router = require("./routes/index");
 const bodyParser = require("body-parser");
 const passport = require("./config/passport");
+const cookieParser = require("cookie-parser");
+const session  = require("express-session");
+const MongoStore  = require("connect-mongo")(session);
+const flash = require("connect-flash");
 
 // Habilitar el archivo de variables de entorno
 require("dotenv").config({ path: ".env" });
@@ -18,10 +23,32 @@ app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 
 app.set("view engine", "hbs");
 
+//Crear la sesion de usuario y la cookie encargada de almacenarla
+app.use(cookieParser());
+
+app.use(
+    session({
+        secret: process.env.SECRET,
+        key: process.env.KEY,
+        resave: false,
+        saveUninitialized: false,
+        store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    })
+);
 
 //Habilitar passport y la estrategia local
 app.use(passport.initialize());
 app.use(passport.session());
+
+//Habilitar los mensajes flash
+app.use(flash());
+
+//Midleware personalizado para agregar mensajes flash
+app.use((req, res, next) => {
+    res.locals.messages = req.flash();
+
+    next();
+})
 
 // Habilitar body-parser para obtener el cuerpo de la petición
 app.use(bodyParser.urlencoded({ extended: true }));
