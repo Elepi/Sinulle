806fdb6dd8853000408a26bcbcdbd0874ce129a9
diegolopez03 <diego232.lopez@gmail.com>
2020-11-02 18:13:27 -0600

    Implementación de bcrypt y realización de funciones pre para realizar el hash de la contraseña

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcrypt");
 
 const usuarioSchema = new mongoose.Schema({
     email: {
@@ -25,5 +25,39 @@ const usuarioSchema = new mongoose.Schema({
     activo: Boolean,
     fechaRegistro: Date
 });
+
+//Hooks hash para el password, combinación de hash y salt 
+usuarioSchema.pre("save", function(next) {
+    const user = this;
+
+    //Si el password fue modificado
+    if(!user.isModified("password")) {
+        return next();
+    }
+
+    //Generar salt, sino da error en su generación se genera el hash también.
+    bcrypt.genSalt(10, (err, salt) => {
+        //Si existe error, continuar
+        if(err) return next(err);
+
+        //Si se genera el salt, generar el hash
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if(err) return next(err);
+
+            /// Se almacena el hash en password
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+//Hooks para agregar al usuario los campos de activo y fecha de su registro
+usuarioSchema.pre("save", function(next) {
+    const user = this;
+    user.activo = true;
+    user.fechaRegistro = Date.now();
+    next();
+});
+
 
 module.exports = mongoose.model("Usuarios", usuarioSchema);
