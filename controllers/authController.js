7b4a07235847d39a36ae0909b62b84c10af97af7
David <davidwelchez@gmail.com>
2020-11-10 19:57:10 -0600

    Controller para el cambio de contraseña finalizado

--- controllers/authController.js
+++ controllers/authController.js
@@ -41,7 +41,7 @@ exports.formularioRestablecerPassword = (req, res, next) => {
   exports.enviarToken = async (req, res, next) => {
     // Obtener la direccción de correo electrónico
     const { email } = req.body;
-    const messages = { messages: [] };
+    const messages = [];
   
     // Buscar el usuario
     try {
@@ -49,13 +49,13 @@ exports.formularioRestablecerPassword = (req, res, next) => {
   
       // El usuario no existe
       if (!usuario) {
-        messages.messages.push({
+        messages.push({
           message:
             "¡No existe una cuenta registrado con este correo electrónico!",
           alertType: "danger",
         });
   
-        req.flash(messages);
+        req.flash("messages", messages);
   
         res.redirect("/olvide-password");
       }
@@ -84,21 +84,122 @@ exports.formularioRestablecerPassword = (req, res, next) => {
       }
   
       // Redireccionar al inicio de sesión
-      messages.messages.push({
+      messages.push({
         message: "¡Verifica tu bandeja de entrada y sigue las instrucciones!",
         alertType: "success",
       });
   
-      req.flash(messages);
+      req.flash("messages", messages);
   
-      res.redirect("/iniciar-sesion");
-    } catch (error) {
-      messages.messages.push({
+      res.redirect("/olvide-password");
+  } catch (error) {
+    messages.push({
+      message:
+        "Ocurrió un error al momento de comunicarse con el servidor. Favor intentar nuevamente.",
+      alertType: "danger",
+    });
+
+    req.flash("messages", messages);
+
+    res.redirect("/olvide-password");
+  }
+};
+
+// Mostrar el formulario de cambio de contraseña
+exports.formularioNuevoPassword = async (req, res, next) => {
+  const messages = [];
+  // Buscar el usuario por medio del token que se envía como parámetro
+  try {
+    // Mongoose se encarge de verificar la validez del token
+    // $gt -> mayor que
+    const usuario = await Usuario.findOne({
+      token: req.params.token,
+      expira: { $gt: Date.now() },
+    });
+
+    // No se pudo encontrar el usuario o el token ha vencido
+    if (!usuario) {
+      messages.push({
         message:
-          "Ocurrió un error al momento de comunicarse con el servidor. Favor intentar nuevamente.",
+          "Solicitud expirada. Vuelve a solicitar el cambio de contraseña.",
         alertType: "danger",
       });
-  
-      req.flash(messages);
+
+      req.flash("messages", messages);
+
+      res.redirect("/olvide-password");
     }
-  };
+
+    // Mostrar el formulario de nuevo password
+    res.render("nuevoPassword", {
+      layout: "main",
+      typePage: "register-page",
+      signButtonValue: "/iniciar-sesion",
+      signButtonText: "Iniciar sesión",
+      year: new Date().getFullYear(),
+    });
+  } catch (error) {
+    messages.push({
+      message:
+        "Ocurrió un error al momento de comunicarse con el servidor. Favor intentar nuevamente.",
+      alertType: "danger",
+    });
+
+    req.flash("messages", messages);
+
+    res.redirect("/olvide-password");
+  }
+};
+
+// Almacena la nueva contraseña del usuario
+exports.almacenarNuevaPassword = async (req, res, next) => {
+  const messages = [];
+
+  try {
+    // Buscar el usuario por medio del token y validar que aún no haya vencido
+    const usuario = await Usuario.findOne({
+      token: req.params.token,
+      expira: { $gt: Date.now() },
+    });
+
+    // No se encontró el token o token vencido
+    if (!usuario) {
+      messages.push({
+        message:
+          "Solicitud expirada. Vuelve a solicitar el cambio de contraseña.",
+        alertType: "danger",
+      });
+
+      req.flash("messages", messages);
+
+      res.redirect("/olvide-password");
+    }
+
+    // Cambiar la contraseña
+    usuario.password = req.body.password;
+
+    // Eliminar los valores que ya no son útiles
+    usuario.token = undefined;
+    usuario.expira = undefined;
+
+    await usuario.save();
+
+    // Redireccionar
+    messages.push({
+      message: "¡Contraseña modificada correctamente!",
+      alertType: "success",
+    });
+    req.flash("messages", messages);
+    res.redirect("/iniciar-sesion");
+  } catch (error) {
+
+    messages.push({
+      message:
+        "Ocurrió un error al momento de comunicarse con el servidor. Favor intentar nuevamente.",
+      alertType: "danger",
+    });
+    req.flash("messages", messages);
+
+    res.redirect("/olvide-password");
+  }
+};
