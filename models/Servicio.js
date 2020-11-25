2858b5aa34dfd5553c6ff6b7d45df5f6e9b822fa
sara991 <yamis704@gmail.com>
2020-11-25 00:32:55 -0600

    Modelo servicio

--- models/Servicio.js
+++ models/Servicio.js
@@ -8,34 +8,14 @@ const servicioSchema = new mongoose.Schema({
   nombre: {
     type: String,
     required: true,
-    trim: true,
+
   }
 });
-//Hooks para agregar al usuario los campos de activo y fecha de su registro
-servicioSchema.pre("save", function (next) {
-    const user = this;
-    user.activo = true;
-    user.fechaRegistro = Date.now();
-    next();
-});
-
-
-
-//Hooks para acceder a los errores de MongoDB (unique key)
-servicioSchema.post("save", function(err, doc, next) {
-    //Verificar si ocurrió un error al momento de almacenar
-    if(err.name == "MongoError" && err.code == 11000) {
-        next("Ya existe el usuario con las dirección de correo electrónico ingresada");
-    } else {
-        next(err);
-    }
-});
-
-
+//Hooks 
 
 
 
 // Generar un índice para mejorar la búsqueda por el nombre del servicio
-servicioSchema.index({ nombre: "text" });
+//servicioSchema.index({ nombre: "text" });
 
-module.exports = mongoose.model("Servicio", servicioSchema);
+module.exports = mongoose.model("Servicios", servicioSchema);
