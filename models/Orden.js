1fe1f6f8f765b9eee348496b90cfb7cea42042e7
Elepi <elenakarenf@gmail.com>
2020-12-01 22:25:47 -0600

    se creo el modelo orden

---
+++ models/Orden.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+//const bcrypt = require("bcryptjs");
+
+const ordenSchema = new mongoose.Schema({
+ cliente:
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "Usuarios", 
+    //required: true,
+      },
+    
+      colaborador: {
+        type: mongoose.Schema.ObjectId,
+        ref: "Usuarios",  
+    //required: true,
+      },
+    
+      servicio: String,
+   
+    //  direccion: String,
+      
+      fechaSolicitud: Date,
+
+      //hora: String,
+
+      descripcion:{
+        type: String,
+        //required: true,
+        trim: true,
+      },
+
+});
+
+
+
+module.exports = mongoose.model("Ordenes", ordenSchema);
