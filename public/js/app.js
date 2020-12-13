46d9fd2c579e4ac68fd149f15729d8869ed692b7
sara991 <yamis704@gmail.com>
2020-12-13 01:06:14 -0600

    Agregar metodo eliminarServicio a app.

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const panelDirecciones = document.querySelector("#panel-direcciones");
     if (panelDirecciones) panelDirecciones.addEventListener("click", eliminarDireccion);
 
+    const panelServicios = document.querySelector("#panel-servicios");
+    if (panelServicios) panelServicios.addEventListener("click", eliminarServicio);
+
 });
 
 const eliminarDireccion = (e) => {
@@ -57,3 +60,52 @@ const eliminarDireccion = (e) => {
         window.location.href == e.target.href;
     } 
 };
+
+const eliminarServicio= (e) => {
+    // Prevenir el comportamiento por defecto
+    e.preventDefault();
+  
+    // Verificar que el botón seleccionado es el botón de eliminar
+    if (e.target.dataset.eliminar) {
+      Swal.fire({
+        title: "¿Está seguro que desea eliminar el servicio?",
+        text: "¡Una vez eliminado, no se puede recuperar!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d330000",
+        confirmButtonText: "Eliminar",
+        cancelButtonText: "Cancelar",
+      }).then((result) => {
+        if (result.value) {
+          // El usuario hace click en eliminar
+          // Redireccionar a la ruta correspondiente
+          const axiosUrl = `${location.origin}/servicio/${e.target.dataset.eliminar}`;
+  
+          // Petición AJAX de eliminación
+          axios
+            .delete(axiosUrl, { id: e.target.dataset.eliminar })
+            .then((response) => {
+              if (response.status == 200) {
+                Swal.fire("¡Eliminado!", response.data, "success");
+  
+                // Eliminar el servicio del DOM
+                e.target.parentElement.parentElement.removeChild(
+                  e.target.parentElement
+                );
+              }
+            })
+            .catch(() => {
+              Swal.fire({
+                type: "error",
+                title: "Error",
+                text: "Ocurrió un error al momento de eliminar el servicio",
+              });
+            });
+        }
+      });
+    } else if (e.target.tagName === "A") {
+      // Si no es el botón de eliminar servicio, realizar la redirección
+      window.location.href = e.target.href;
+    }
+  };
