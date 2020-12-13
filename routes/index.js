3eef2e0ca2b020921c3ac12e79c10ab78d0c826e
David <davidwelchez@gmail.com>
2020-12-12 23:00:41 -0600

    Ruta para el historial de ordenes de servicio

--- routes/index.js
+++ routes/index.js
@@ -209,3 +209,9 @@ usuarioController.eliminarDireccion);
 router.get("/manual", (req, res, next)=> {
   res.render("manual");
 });
+
+
+//Ordenes de usuario
+router.get("/ordenes-usuario", 
+authController.verificarInicioSesion,
+ordenController.ordenUsuario);
